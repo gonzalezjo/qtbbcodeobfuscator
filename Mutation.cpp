479bcc559e97d039f7349998c7e7a16bd84e89b3
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2017-02-03 09:49:49 -0500

    Refactoring (shrinking codebase)

--- Mutation.cpp
+++ Mutation.cpp
@@ -3,11 +3,10 @@
 QString Mutation::operator()(QString p_message)
 {
     m_isOpen = !m_isOpen;
-    p_message = m_function(p_message, m_isOpen);
-    return p_message;
+    return m_function(p_message, m_isOpen);
 }
 
-bool Mutation::open()
+bool Mutation::isOpen()
 {
     return m_isOpen;
 }
