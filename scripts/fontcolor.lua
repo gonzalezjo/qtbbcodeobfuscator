55dfbca89b6d3e1874e86ba6291a63434bb98892
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2017-02-03 10:11:11 -0500

    Update fontcolor.lua

--- scripts/fontcolor.lua
+++ scripts/fontcolor.lua
@@ -1,12 +1,24 @@
 function main(message, opened)
-    local color = ""
+    local color = "#"
     local random
 
-    for i = 1, 6 do
-        random = math.random(48, 70)
-        random = random - ((random > 57 and random < 65) and 7 or 0)
-        color = (color .. string.char(random)) -- random hex!
+    --[[ 
+    -- my method of generating random color codes
+    -- apologies if it's confusing
+    -- but i didn't like any of the methods online
+    -- so i had to make my own 
+    -- i tried to add comments though
+    ]]-- 
+    
+    for i = 1, 6 do 
+        -- random ascii code for 0 - F
+        random = math.random(48, 70) 
+        -- ignore colons, semicolons, etc
+        random = random - ((random > 57 and random < 65) and 7 or 0) 
+        -- append it to the color code
+        color = color .. string.char(random)
     end
 
-    return not opened and "[color=#"..color.."]"..message or message.."[/color]"
+    -- open or close the tag appropriately
+    return opened and "[color="..color.."]"..message or message.."[/color]"
 end
